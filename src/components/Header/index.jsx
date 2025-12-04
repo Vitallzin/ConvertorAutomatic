9919a45d6628b63cf2ea91ca6f52fce1205c7e3f
duduardo11 <eduardonascimento2016@gmail.com>
2025-12-04 17:44:58 -0300

    Adicionado comentários no Header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,38 +1,52 @@
-import React from 'react';
-import './Header.css';
-import ThemeToggle from '../ThemeToggle';
+import React from 'react'; // Necessário para criar o componente
+import './Header.css'; // Arquivo de estilização do header
+import ThemeToggle from '../ThemeToggle'; // Trocar o tema claro/escuro
 
 function Header() {
-    const handleReloadClick = () => {
-    // Recarrega a página atual do navegador
+
+  // Função que recarrega a página inteira ao clicar na logo - "ConvertorAutomatic"
+  const handleReloadClick = () => {
     window.location.reload();
   };
   
   return (
-    <header className="app-header">
-      <div className="header-left">
-        <a onClick={handleReloadClick} className="logo">ConvertorAutomatic</a>
-        <a href="#">Ajuda</a>
+    <header className="app-header">     {/* Layout principal do header */}
+      
+      <div className="header-left">     {/* Lado esquerdo do header */}
+
+        {/* Logo clicável que recarrega a página */}
+        <a onClick={handleReloadClick} className="logo"> 
+          ConvertorAutomatic
+        </a>
+
+        <a href="#"> Ajuda </a>
+
+        {/* Link da API de conversão de moedas */}
         <a 
           href="https://www.exchangerate-api.com/" 
-          target="_blank" 
-          rel="noreferrer"
+          target="_blank"
+          rel="noreferrer"     // Prática de segurança e privacidade
         >
           API Exchange
         </a>
+
+        {/* Link da API para as bandeiras dos países */}
         <a
           href="https://flagsapi.com/#countries"
           target="_blank" 
           rel="noreferrer"
-          >
+        >
           API CountryFlag
         </a>
       </div>
+
+      {/* Lado direito do header */}
       <div className="header-right">
-        <ThemeToggle />
+        <ThemeToggle /> {/* Componente do botão de tema */}
       </div>
+
     </header>
   );
 }
 
-export default Header;
+export default Header;     // Permite que o componente seja importado no App.jsx
